674f8c8878e20222a89682d77ddac13d62d06c28
wervin <fruitaccount@live.com>
2023-05-21 15:10:45 +0200

    typo

--- sake/sake_macro.h
+++ sake/sake_macro.h
@@ -1,10 +1,10 @@
 #ifndef SAKE_MACRO_H
 #define SAKE_MACRO_H
 
-#define SAKE_MACRO_BEGIN  do {
+#define SAKE_MACRO_BEGIN            do {
 
-#define SAKE_MACRO_END    } while(0)
+#define SAKE_MACRO_END              } while(0)
 
-#define SAKE_UNUSED(x)         (void)(x)
+#define SAKE_MACRO_UNUSED(x)        (void)(x)
 
 #endif /* SAKE_MACRO_H */
